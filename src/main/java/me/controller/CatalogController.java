1702b6b66d36e8204649d16b62749d7c8f2a81cf
sombrero104 <sombrero.khy@gmail.com>
2022-07-05 20:55:36 +0900

    .

--- src/main/java/me/controller/CatalogController.java
+++ src/main/java/me/controller/CatalogController.java
@@ -1,13 +1,20 @@
 package me.controller;
 
+import me.jpa.CatalogEntity;
 import me.service.CatalogService;
+import me.vo.ResponseCatalog;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/catalog-service")
@@ -25,6 +32,18 @@ public class CatalogController {
     @GetMapping("/health_check")
     public String status(HttpServletRequest request) {
         return String.format("It's working in catalog service on port %s", request.getServerPort());
+    }
+
+    @GetMapping("/catalogs")
+    public ResponseEntity<List<ResponseCatalog>> getCatalogs() {
+        Iterable<CatalogEntity> catalogs = catalogService.getAllCatalogs();
+
+        List<ResponseCatalog> result = new ArrayList<>();
+        catalogs.forEach(c -> {
+            result.add(new ModelMapper().map(c, ResponseCatalog.class));
+        });
+
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
 }
